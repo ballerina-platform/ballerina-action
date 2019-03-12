080cb41dece5d3d4be69e0af28cbd6a29ae038d4
Anjana Fernando <2379767+lafernando@users.noreply.github.com>
2019-03-11 18:50:41 -0700

    Update entrypoint.sh

--- cli/latest/entrypoint.sh
+++ cli/latest/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-echo $DOCKER_HOST
-ls /var/run/docker.sock
 echo "XXXXXX"
 env
-docker pull hello-world
+ls /var/run/docker.sock
+/ballerina/runtime/bin/ballerina $*
+cat ballerina-internal.log
