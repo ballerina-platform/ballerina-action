6093702d585eafa5b9a875fa4e28265d66214271
Anjana Fernando <2379767+lafernando@users.noreply.github.com>
2019-03-11 18:11:10 -0700

    Update entrypoint.sh

--- cli/latest/entrypoint.sh
+++ cli/latest/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
 echo $DOCKER_HOST
-$BALLERINA_HOME/bin/ballerina $*
+/ballerina/runtime/bin/ballerina $*
+echo $DOCKER_HOST
 cat ballerina-internal.log
 ls /tmp -la
 touch /tmp/x.txt
